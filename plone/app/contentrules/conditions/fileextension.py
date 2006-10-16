6b00d894cec069c9c125f9a2fbd5774c976a0dda
wayworn <wayworn@localhost>
2006-10-16 14:23:17 +0000

    added file extension condition, minor other change
    svn path=/plone.app.contentrules/trunk/; revision=11168

--- plone/app/contentrules/conditions/fileextension.py
+++ plone/app/contentrules/conditions/fileextension.py
@@ -11,32 +11,32 @@ from plone.contentrules.rule.rule import Node
 
 from plone.app.contentrules.browser.formhelper import AddForm, EditForm 
 
-class IPortalTypeCondition(IRuleConditionData):
+class IFileExtensionCondition(IRuleConditionData):
     """Interface for the configurable aspects of a portal type condition.
     
     This is also used to create add and edit forms, below.
     """
     
-    portal_type = schema.TextLine(title=u"Portal type",
-                                  description=u"The name of the portal type, as found in the portal_types tool",
+    file_extension = schema.TextLine(title=u"File extension",
+                                  description=u"The file extension to check for",
                                   required=True)
          
-class PortalTypeCondition(SimpleItem):
-    """The actual persistent implementation of the portal type condition element.
+class FileExtensionCondition(SimpleItem):
+    """The actual persistent implementation of the logger action element.
     
     Note that we must mix in Explicit to keep Zope 2 security happy.
     """
-    implements(IPortalTypeCondition)
+    implements(IFileExtensionCondition)
     
-    portal_type = u''
+    file_extension = u''
 
-class PortalTypeConditionExecutor(object):
+class FileExtensionConditionExecutor(object):
     """The executor for this condition.
     
     This is registered as an adapter in configure.zcml
     """
     implements(IExecutable)
-    adapts(Interface, IPortalTypeCondition, Interface)
+    adapts(Interface, IFileExtensionCondition, Interface)
          
     def __init__(self, context, element, event):
         self.context = context
@@ -44,29 +44,40 @@ class PortalTypeConditionExecutor(object):
         self.event = event
 
     def __call__(self):
-        typeInfo = getattr(self.event.object, 'getTypeInfo', None)
-        if typeInfo is None:
+        kind = getattr(self.event.object, 'getPortalTypeName', None)()
+
+        # Only act on files; folders etc have no "filename"
+        if kind != 'File':
             return False
-        return typeInfo().getId() == self.element.portal_type
         
-class PortalTypeAddForm(AddForm):
-    """An add form for portal type rule conditions.
+        name = getattr(self.event.object, 'getFilename', None)()
+        extension = name[name.rfind('.')+1:]
+        
+        if extension == self.element.file_extension:
+            import pdb; pdb.set_trace()
+            
+            return True
+        
+        return False
+        
+class FileExtensionAddForm(AddForm):
+    """An add form for file extension rule conditions.
     
     Note that we create a Node(), not just a LoggerAction, since this is what
     the elements list of IRule expects. The namespace traversal adapter
     (see traversal.py) takes care of unwrapping the actual instance from
     a Node when it's needed.
     """
-    form_fields = form.FormFields(IPortalTypeCondition)
+    form_fields = form.FormFields(IFileExtensionCondition)
     
     def create(self, data):
-        c = PortalTypeCondition()
-        c.portal_type = data.get('portal_type')
-        return Node('plone.conditions.PortalType', c)
+        c = FileExtensionCondition()
+        c.file_extension = data.get('file_extension')
+        return Node('plone.conditions.FileExtension', c)
 
-class PortalTypeEditForm(EditForm):
+class FileExtensionEditForm(EditForm):
     """An edit form for portal type conditions
     
     Formlib does all the magic here.
     """
-    form_fields = form.FormFields(IPortalTypeCondition)
+    form_fields = form.FormFields(IFileExtensionCondition)
