b3812bbe3bc32caf8ca1c68528a34f8f831b7c2a
Martin Aspeli <optilude@gmail.com>
2006-12-28 21:34:25 +0000

    Using IObjectAddedEvent is no good (see TODO.txt); for now, we will only be able to support AT objects for "new object" rules.
    svn path=/plone.app.contentrules/trunk/; revision=11762

--- plone/app/contentrules/handlers.py
+++ plone/app/contentrules/handlers.py
@@ -3,8 +3,15 @@ from plone.contentrules.engine.interfaces import IRuleExecutor
 
 from Acquisition import aq_inner, aq_parent
             
+# XXX: Disabled for now, see comment in configure.zcml
 def added(event):
     container = IRuleContainer(event.newParent, None)
+    if container is not None:
+        executor = IRuleExecutor(container)
+        executor.executeAll(event)
+        
+def initialized(event):
+    container = IRuleContainer(aq_parent(aq_inner(event.object)), None)
     if container is not None:
         executor = IRuleExecutor(container)
         executor.executeAll(event)
