e8d61f1a2454f81d9a130098d0810d2dc0b37a6c
Martin Aspeli <optilude@gmail.com>
2006-12-29 14:49:05 +0000

    Update to reflect reality
    svn path=/plone.app.contentrules/trunk/; revision=11771

--- plone/app/contentrules/tests/test_setup.py
+++ plone/app/contentrules/tests/test_setup.py
@@ -1,8 +1,7 @@
-from zope.lifecycleevent.interfaces import IObjectCreatedEvent
-from zope.lifecycleevent.interfaces import IObjectCopiedEvent
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
-from zope.app.container.interfaces import IObjectAddedEvent
+#from zope.app.container.interfaces import IObjectAddedEvent
 from zope.app.container.interfaces import IObjectRemovedEvent
+from Products.Archetypes.interfaces import IObjectInitializedEvent
 
 from plone.contentrules.engine.interfaces import IRuleContainer
 from plone.contentrules.rule.interfaces import IRuleEventType
@@ -16,10 +15,9 @@ class TestProductInstall(ContentRulesTestCase):
         self.failUnless(IRuleContainer.providedBy(self.portal))
         
     def testEventTypesMarked(self): 
-        self.failUnless(IRuleEventType.providedBy(IObjectCreatedEvent))
-        self.failUnless(IRuleEventType.providedBy(IObjectCopiedEvent))
+        self.failUnless(IRuleEventType.providedBy(IObjectInitializedEvent))
         self.failUnless(IRuleEventType.providedBy(IObjectModifiedEvent))
-        self.failUnless(IRuleEventType.providedBy(IObjectAddedEvent))
+        # self.failUnless(IRuleEventType.providedBy(IObjectAddedEvent))
         self.failUnless(IRuleEventType.providedBy(IObjectRemovedEvent))
         
 def test_suite():
