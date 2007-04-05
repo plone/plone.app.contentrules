e214d01206bb3f51bd1e1deaa3a33b2c3b326d90
Jean-Francois Roche <jfroche@jfroche.be>
2007-04-05 00:36:37 +0000

    Added Mail ContentRules + tests, this refs #6139
    svn path=/plone.app.contentrules/trunk/; revision=14173

---
+++ plone/app/contentrules/actions/mail.py
@@ -0,0 +1,128 @@
+from OFS.SimpleItem import SimpleItem
+from zope.component import adapts, queryUtility, getUtility
+from zope.component.interfaces import ComponentLookupError
+from zope.interface import Interface, implements
+from zope.formlib import form
+from zope import schema
+
+from plone.app.contentrules.browser.formhelper import AddForm, EditForm 
+from plone.contentrules.rule.interfaces import IRuleElementData, IExecutable
+
+from Products.CMFCore.interfaces import IURLTool
+from Products.CMFPlone import PloneMessageFactory as _
+from Products.MailHost.interfaces import IMailHost
+
+
+class IMailAction(Interface):
+    """Definition of the configuration available for a mail action
+    """
+    subject = schema.TextLine(title=_(u"Subject"),
+                              description=_(u"Subject of the message"),
+                              required=True)
+    source = schema.TextLine(title=_(u"Email source"),
+                             description=_("The email address that send the \
+email. If no email is provided here, it will use the portal from address."),
+                             required=False)
+    recipients = schema.TextLine(title=_(u"Email recipients"),
+                                description=_("The email where you want to \
+send this message. To send it to different email addresses, just separate them\
+ with ,"),
+                                required=True)
+    message = schema.Text(title=_(u"Message"),
+                          description=_(u"Type in here the message that you \
+want to mail. Some defined content can be replaced: ${title} will be replaced \
+by tahe title of the newly created item. ${url} will be replaced by the \
+title of the newly created item"),
+                          required=True)
+
+class MailAction(SimpleItem):
+    """
+    The implementation of the action defined before
+    """
+    implements(IMailAction, IRuleElementData)
+
+    subject = ''
+    source = ''
+    recipients = ''
+    message = ''
+
+    element = 'plone.actions.Mail'
+
+    @property
+    def summary(self):
+        return _(u"Email report to ${recipients}",
+                 mapping=dict(recipients=self.recipients))
+
+
+class MailActionExecutor(object):
+    """The executor for this action.
+    """
+    implements(IExecutable)
+    adapts(Interface, IMailAction, Interface)
+
+    def __init__(self, context, element, event):
+        self.context = context
+        self.element = element
+        self.event = event
+
+    def __call__(self):
+        recipients = [str(mail.strip()) for mail in \
+                      self.element.recipients.split(',')]
+        mailhost = queryUtility(IMailHost)
+        if not mailhost:
+            raise ComponentLookupError, 'You must have a Mailhost utility to \
+execute this action'
+
+        source = str(self.element.source)
+        urltool = getUtility(IURLTool)
+        portal = urltool.getPortalObject()
+        email_charset = portal.getProperty('email_charset')
+
+        if not source:
+            # no source provided, looking for the site wide from email
+            # address
+            from_address = portal.getProperty('email_from_address')
+            if not from_address:
+                raise ValueError, 'You must provide a source address for this\
+action or enter an email in the portal properties'
+            from_name = portal.getProperty('email_from_name')
+            source = "%s <%s>" % (from_address, portal.email_from_address)
+
+        obj = self.event.object
+        event_title = obj.Title()
+        event_url = obj.absolute_url()
+        message = self.element.message.replace("${url}", event_url)
+        message = message.replace("${title}", event_title)
+
+        subject = self.element.subject.replace("${url}", event_url)
+        subject = subject.replace("${title}", event_title)
+
+        for email_recipient in recipients:
+            mailhost.secureSend(message, email_recipient, source,
+                                subject=subject, subtype='plain',
+                                charset=email_charset, debug=False,
+                                From=source)
+        return True
+
+class MailAddForm(AddForm):
+    """
+    An add form for the mail action
+    """
+    form_fields = form.FormFields(IMailAction)
+    label = _(u"Add Mail action")
+    description = _(u"A Mail action can mail different recipient.")
+    form_name = _(u"Configure element")
+
+    def create(self, data):
+        a = MailAction()
+        form.applyChanges(a, self.form_fields, data)
+        return a
+
+class MailEditForm(EditForm):
+    """
+    An edit form for the mail action
+    """
+    form_fields = form.FormFields(IMailAction)
+    label = _(u"Edit Mail Action")
+    description = _(u"A Mail action can mail different recipient.")
+    form_name = _(u"Configure element")
---
+++ plone/app/contentrules/tests/test_action_mail.py
@@ -0,0 +1,130 @@
+from email.MIMEText import MIMEText
+from zope.component import getUtility, getMultiAdapter, getSiteManager
+from zope.component.interfaces import IObjectEvent
+from zope.interface import implements
+
+from plone.app.contentrules.rule import Rule
+from plone.app.contentrules.tests.base import ContentRulesTestCase
+from plone.app.contentrules.actions.mail import MailAction, MailEditForm, MailAddForm
+from plone.contentrules.engine.interfaces import IRuleStorage
+from plone.contentrules.rule.interfaces import IRuleAction, IExecutable
+
+from Products.MailHost.interfaces import IMailHost
+from Products.SecureMailHost.SecureMailHost import SecureMailHost
+
+class DummyEvent(object):
+    implements(IObjectEvent)
+
+    def __init__(self, object):
+        self.object = object
+
+class DummySecureMailHost(SecureMailHost):
+    meta_type = 'Dummy secure Mail Host'
+    def __init__(self, id):
+        self.id = id
+        self.sent = []
+
+    def _send(self, mfrom, mto, messageText, debug=False):
+        self.sent.append(messageText)
+
+
+class TestMailAction(ContentRulesTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager',))
+        self.portal.invokeFactory('Folder', 'target')
+        self.folder.invokeFactory('Document', 'd1', title='Welcome in Plone')
+
+    def testRegistered(self):
+        element = getUtility(IRuleAction, name='plone.actions.Mail')
+        self.assertEquals('plone.actions.Mail', element.addview)
+        self.assertEquals('edit', element.editview)
+        self.assertEquals(None, element.for_)
+        self.assertEquals(IObjectEvent, element.event)
+
+    def testInvokeAddView(self):
+        element = getUtility(IRuleAction, name='plone.actions.Mail')
+        storage = getUtility(IRuleStorage)
+        storage[u'foo'] = Rule()
+        rule = self.portal.restrictedTraverse('++rule++foo')
+
+        adding = getMultiAdapter((rule, self.portal.REQUEST), name='+action')
+        addview = getMultiAdapter((adding, self.portal.REQUEST),
+                                  name=element.addview)
+        self.failUnless(isinstance(addview,MailAddForm))
+
+        addview.createAndAdd(data={'subject' : 'My Subject',
+                                   'source': 'foo@bar.be',
+                                   'recipients': 'foo@bar.be,bar@foo.be',
+                                   'message': 'Hey, Oh!'})
+
+        e = rule.actions[0]
+        self.failUnless(isinstance(e, MailAction))
+        self.assertEquals('My Subject', e.subject)
+        self.assertEquals('foo@bar.be', e.source)
+        self.assertEquals('foo@bar.be,bar@foo.be', e.recipients)
+        self.assertEquals('Hey, Oh!', e.message)
+
+    def testInvokeEditView(self):
+        element = getUtility(IRuleAction, name='plone.actions.Mail')
+        e = MailAction()
+        editview = getMultiAdapter((e, self.folder.REQUEST),
+                                   name=element.editview)
+        self.failUnless(isinstance(editview, MailEditForm))
+
+    def testExecute(self):
+        self.loginAsPortalOwner()
+        sm = getSiteManager(self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        dummyMailHost = DummySecureMailHost('dMailhost')
+        sm.registerUtility(dummyMailHost, IMailHost)
+        e = MailAction()
+        e.source = "foo@bar.be"
+        e.recipients = "bar@foo.be"
+        e.message = "Document '${title}' created in ${url} !"
+        ex = getMultiAdapter((self.folder, e, DummyEvent(self.folder.d1)),
+                             IExecutable)
+        ex()
+        self.failUnless(isinstance(dummyMailHost.sent[0], MIMEText))
+        mailSent = dummyMailHost.sent[0]
+        self.assertEqual('text/plain; charset="utf-8"',
+                        mailSent.get('Content-Type'))
+        self.assertEqual("bar@foo.be", mailSent.get('To'))
+        self.assertEqual("foo@bar.be", mailSent.get('From'))
+        self.assertEqual("Document 'Welcome in Plone' created in \
+http://nohost/plone/Members/test_user_1_/d1 !",
+                         mailSent.get_payload(decode=True))
+
+    def testExecuteMultiRecipients(self):
+        self.loginAsPortalOwner()
+        sm = getSiteManager(self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        dummyMailHost = DummySecureMailHost('dMailhost')
+        sm.registerUtility(dummyMailHost, IMailHost)
+        e = MailAction()
+        e.source = 'foo@bar.be'
+        e.recipients = 'bar@foo.be,foo@bar.be'
+        e.message = 'Document created !'
+        ex = getMultiAdapter((self.folder, e, DummyEvent(self.folder.d1)),
+                             IExecutable)
+        ex()
+        self.assertEqual(len(dummyMailHost.sent), 2)
+        self.failUnless(isinstance(dummyMailHost.sent[0], MIMEText))
+        mailSent = dummyMailHost.sent[0]
+        self.assertEqual('text/plain; charset="utf-8"',
+                        mailSent.get('Content-Type'))
+        self.assertEqual('bar@foo.be', mailSent.get('To'))
+        self.assertEqual('foo@bar.be', mailSent.get('From'))
+        self.assertEqual('Document created !',mailSent.get_payload(decode=True))
+        mailSent = dummyMailHost.sent[1]
+        self.assertEqual('text/plain; charset="utf-8"',
+                        mailSent.get('Content-Type'))
+        self.assertEqual('foo@bar.be', mailSent.get('To'))
+        self.assertEqual('foo@bar.be', mailSent.get('From'))
+        self.assertEqual('Document created !',mailSent.get_payload(decode=True))
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestMailAction))
+    return suite
