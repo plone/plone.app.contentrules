28d3644e65b06c02f7241408a725f44790a4905a
Martin Aspeli <optilude@gmail.com>
2007-07-02 17:53:35 +0000

    Don't display the content rules tab if rules are disabled. Closes #6449.
    svn path=/plone.app.contentrules/trunk/; revision=15680

--- plone/app/contentrules/browser/interfaces.py
+++ plone/app/contentrules/browser/interfaces.py
@@ -1,6 +1,14 @@
 from zope.interface import Interface
 from zope.app.container.interfaces import IAdding
 
+class IContentRulesInfo(Interface):
+    """Site-wide information about content rules
+    """
+    
+    def show_rules_tab():
+        """Determine whether or not the rules tab should be shown.
+        """
+
 class IRuleAdding(IAdding):
     """Marker interface for rule add views.
     
