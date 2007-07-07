2cbd6266640f5760d5d81cb5e507c6e1574012fc
Hanno Schlichting <hanno@hannosch.eu>
2007-07-07 22:43:53 +0000

    Whitespace
    svn path=/plone.app.contentrules/trunk/; revision=15813

--- plone/app/contentrules/browser/adding.py
+++ plone/app/contentrules/browser/adding.py
@@ -5,7 +5,6 @@ from zope.component import getMultiAdapter, getUtility
 from zope.app.container.interfaces import INameChooser
 
 from Acquisition import aq_base, aq_inner, aq_parent, Implicit
-from OFS.SimpleItem import SimpleItem
 from Products.Five import BrowserView
 
 from plone.contentrules.engine.interfaces import IRuleStorage
@@ -15,6 +14,7 @@ from plone.app.contentrules.browser.interfaces import IRuleConditionAdding
 from plone.app.contentrules.browser.interfaces import IRuleActionAdding
 
 class RuleAdding(Implicit, BrowserView):
+
     implements(IRuleAdding)
 
     def add(self, content):
@@ -23,7 +23,7 @@ class RuleAdding(Implicit, BrowserView):
         storage = getUtility(IRuleStorage)
         chooser = INameChooser(storage)
         storage[chooser.chooseName(None, content)] = content
-        
+
     def nextURL(self):
         url = str(getMultiAdapter((self.context, self.request), name=u"absolute_url"))
         return url + "/@@rules-controlpanel"
@@ -39,7 +39,7 @@ class RuleAdding(Implicit, BrowserView):
         return False
 
 class RuleElementAdding(Implicit, BrowserView):
-        
+
     def nextURL(self):
         url = str(getMultiAdapter((aq_parent(self.context), self.request), name=u"absolute_url"))
         return url + "/@@manage-content-rules"
@@ -53,19 +53,21 @@ class RuleElementAdding(Implicit, BrowserView):
 
     def nameAllowed(self):
         return False
-        
+
 class RuleConditionAdding(RuleElementAdding):
+
     implements(IRuleConditionAdding)
-    
+
     def add(self, content):
         """Add the rule element to the context rule
         """
         rule = aq_base(aq_inner(self.context))
         rule.conditions.append(content)
-        
+
 class RuleActionAdding(RuleElementAdding):
+
     implements(IRuleActionAdding)
-    
+
     def add(self, content):
         """Add the rule element to the context rule
         """
