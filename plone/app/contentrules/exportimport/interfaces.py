4374ec09bb415d49abe3a2b69d115723ca26b485
Martin Aspeli <optilude@gmail.com>
2007-12-22 02:45:39 +0000

    Working contentrules import/export and purge :)
    svn path=/plone.app.contentrules/branches/plip204-contentrules-gs/; revision=18513

--- plone/app/contentrules/exportimport/interfaces.py
+++ plone/app/contentrules/exportimport/interfaces.py
@@ -5,13 +5,14 @@ class IRuleElementExportImportHandler(Interface):
     for a particular content rule element (condition or action)
     """
     
-    def import_element(interface, node):
+    def import_element(node):
         """Set the properties on the given element, based on the given
         element schema interface. The node is the <condition /> or <action /> 
         root node. Settings are expected to be found in children of the node.
         """
         
-    def export_element(interface, fragment):
+    def export_element(doc, node):
         """Export the properties of the given element with the given 
-        element schema interface into the XML document fragment.
+        element schema interface and append them to the given root node.
+        Use the doc object to create new nodes.
         """
