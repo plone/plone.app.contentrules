4b066402847119c3b3c98e8c95b539d7d86015e2
Thomas Desvenain <thomas.desvenain@gmail.com>
2010-09-03 12:20:23 +0000

    fix duplicate rule filter crashed at site root level   in Acquisition Wrapper case.   closes http://dev.plone.org/plone/ticket/10597.
    svn path=/plone.app.contentrules/trunk/; revision=39626

--- plone/app/contentrules/handlers.py
+++ plone/app/contentrules/handlers.py
@@ -17,7 +17,7 @@ class DuplicateRuleFilter(object):
     """A filter which can prevent rules from being executed more than once
     regardless of context.
     """
-    
+
     def __init__(self):
         self.reset()
 
@@ -29,10 +29,12 @@ class DuplicateRuleFilter(object):
         obj = context
         if IObjectEvent.providedBy(event):
             obj = event.object
-        
+
         uid_method = getattr(obj, 'UID', None)
         if uid_method is not None:
             uid = uid_method()
+        elif ISiteRoot.providedBy(context):
+            uid = context.id
         else:
             uid = '/'.join(context.getPhysicalPath())
         if (uid, rule.__name__,) in self.executed:
@@ -57,37 +59,37 @@ def close(event):
         _status.rule_filter.reset()
     if hasattr(_status, 'delayed_events'):
         _status.delayed_events = {}
-    
+
 def execute(context, event):
     """Execute all rules relative to the context, and bubble as appropriate.
     """
-    
+
     # Do nothing if there is no rule storage or it is not active
     storage = queryUtility(IRuleStorage)
     if storage is None or not storage.active:
         return
-    
+
     init()
-    
+
     rule_filter = _status.rule_filter
-    
+
     # Stop if someone else is already executing. This could happen if,
     # for example, a rule triggered here caused another event to be fired.
     if rule_filter.in_progress:
         return
-        
+
     # Tell other event handlers to be equally kind
     rule_filter.in_progress = True
-    
+
     # Prepare to break hard if a rule demanded execution be stopped
     try:
-    
+
         # Try to execute rules in the context. It may not work if the context
         # is not a rule executor, but we may still want to bubble events
         executor = IRuleExecutor(context, None)
         if executor is not None:
             executor(event, bubbled=False, rule_filter=rule_filter)
-    
+
         # Do not bubble beyond the site root
         if not ISiteRoot.providedBy(context):
             parent = aq_parent(aq_inner(context))
@@ -102,12 +104,12 @@ def execute(context, event):
 
     except StopRule:
         pass
-    
+
     # We are done - other events that occur after this one will be allowed to
     # execute rules again
     rule_filter.in_progress = False
 
-    
+
 # Event handlers
 
 def is_portal_factory(context):
@@ -126,10 +128,10 @@ def added(event):
     """
     if is_portal_factory(event.object):
         return
-    
+
     # The object added event executes too early for Archetypes objects.
     # We need to delay execution until we receive a subsequent IObjectInitializedEvent
-    
+
     if not IBaseObject.providedBy(event.object):
         execute(event.newParent, event)
     else:
@@ -142,7 +144,7 @@ def archetypes_initialized(event):
     """
     if is_portal_factory(event.object):
         return
-    
+
     if not IBaseObject.providedBy(event.object):
         return
 
@@ -151,34 +153,34 @@ def archetypes_initialized(event):
     if delayed_event is not None:
         _status.delayed_events[IObjectInitializedEvent] = None
         execute(delayed_event.newParent, delayed_event)
-        
+
 def removed(event):
     """When an IObjectRemevedEvent was received, execute rules assigned to its
      previous parent.
     """
-    
+
     if is_portal_factory(event.object):
         return
-        
+
     execute(event.oldParent, event)
-    
+
 def modified(event):
     """When an object is modified, execute rules assigned to its parent
     """
-    
+
     if is_portal_factory(event.object):
         return
-    
+
     # Let the special handler take care of IObjectInitializedEvent
     if not IObjectInitializedEvent.providedBy(event):
         execute(aq_parent(aq_inner(event.object)), event)
-        
+
 def workflow_action(event):
     """When a workflow action is invoked on an object, execute rules assigned
     to its parent.
     """
-    
+
     if is_portal_factory(event.object):
         return
-    
+
     execute(aq_parent(aq_inner(event.object)), event)
