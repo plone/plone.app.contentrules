378852d28712671d7b8fabb31148fe1753e7de5c
tdesvenain <thomas.desvenain@gmail.com>
2012-10-13 16:11:27 +0200

    pdb...

--- plone/app/contentrules/browser/resources.py
+++ plone/app/contentrules/browser/resources.py
@@ -4,7 +4,6 @@ from plone.app.layout.viewlets.common import ViewletBase
 class Resources(ViewletBase):
 
     def render(self):
-        import pdb;pdb.set_trace()
         return u"""
       <script type="text/javascript" src="%(portal_url)s/++resource++manage-contentrules.js"></script>
       <link rel="stylesheet" type="text/css" media="all" href="%(portal_url)s/++resource++manage-contentrules.css"></link>
